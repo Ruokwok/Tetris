f833d830ed5c2014ef0398ffc820d2928b35d209
Ruokwok <i@ruok.cc>
2023-02-20 20:19:46 +0800

    新增帮助命令

--- src/main/java/cc/ruok/tetris/commands/TetrisCommand.java
+++ src/main/java/cc/ruok/tetris/commands/TetrisCommand.java
@@ -17,22 +17,38 @@ public class TetrisCommand extends Command {
         if (commandSender.isPlayer()) {
             Player player = (Player) commandSender;
             if (args.length > 0) {
-                if (args[0].equals("set")) {
-                    if (player.isOp()) {
-                        TetrisSetting.getInstance().setting(player);
-                    } else {
-                        return false;
-                    }
-                } else if (args[0].equals("play")) {
-                    player.sendMessage("即将开始");
-                    player.sendMessage("左右行走控制移动  跳跃控制旋转");
-                    if (!TetrisGame.getStats()) {
-                        new Thread(() -> TetrisGame.start(player)).start();
-                        return true;
-                    } else {
-                        commandSender.sendMessage("游戏正在进行");
-                        return false;
-                    }
+                switch (args[0]) {
+                    case "set":
+                        if (player.isOp()) {
+                            TetrisSetting.getInstance().setting(player);
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case "config":
+                        if (player.isOp()) {
+                            TetrisSetting.config(player);
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case "play":
+                        if (!TetrisGame.getStats()) {
+                            new Thread(() -> TetrisGame.start(player)).start();
+                            return true;
+                        } else {
+                            commandSender.sendMessage("游戏正在进行");
+                            return false;
+                        }
+                    case "help":
+                        player.sendMessage("[俄罗斯方块] 帮助");
+                        player.sendMessage("方向键 - 左右移动");
+                        player.sendMessage("前进/跳跃 - 旋转");
+                        player.sendMessage("后退 - 下落");
+                        player.sendMessage("/tetris play - 开始游戏");
+                        if (player.isOp()) {
+                            player.sendMessage("/tetris set - 俄罗斯方块设置");
+                        }
                 }
 
             }
