fb25df23d98477ed62c7e884d9b7b56536289d5b
Ruokwok <i@ruok.cc>
2023-08-22 22:22:14 +0800

    定时刷新排行榜任务类

---
+++ src/main/java/cc/ruok/tetris/tasks/UpdateRankingTask.java
@@ -0,0 +1,24 @@
+package cc.ruok.tetris.tasks;
+
+import cc.ruok.tetris.Ranking;
+import cc.ruok.tetris.Tetris;
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.scheduler.Task;
+
+import java.util.Collection;
+
+public class UpdateRankingTask extends Task {
+
+    @Override
+    public void onRun(int i) {
+        if (Tetris.tetris.config.ranking != null) {
+            Collection<Player> players = Server.getInstance().getOnlinePlayers().values();
+            for (Player player : players) {
+                if (Tetris.tetris.config.level.equals(player.getLevel().getName())) {
+                    Ranking.updateFloatingText(player);
+                }
+            }
+        }
+    }
+}
